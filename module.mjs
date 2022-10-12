098767388269732bec8bf73afa7f76fcad589601
alanc11 <castillo.alan911@gmail.com>
2022-10-11 19:48:06 -0500

    a Aplicando modulos
    git commit -m a

---
+++ module.mjs
@@ -0,0 +1,15 @@
+import{ PlatziClass } from "./main.mjs";
+
+const clase67 = new PlatziClass({
+    name: "JavaScript orientado a objetos",
+    videoID: "ljouh13jnd",
+});
+ 
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
